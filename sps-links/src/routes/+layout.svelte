32bea572533f695889a6ad6a619c7b0ad86b3f17
heheeheeha <maildrop0001@gmail.com>
2025-10-14 23:34:20 +0100

    version 1 of refactor

--- sps-links/src/routes/+layout.svelte
+++ sps-links/src/routes/+layout.svelte
@@ -1,11 +1,46 @@
 <script>
-	import favicon from '$lib/assets/favicon.svg';
-
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+		rel="stylesheet"
+	/>
+
+	<title>SPS Links</title>
+
+	<link
+		rel="icon"
+		href="https://www.stpaulsschool.org.uk/wp-content/uploads/2019/05/St-Pauls-Logo-150x150.png"
+		sizes="32x32"
+	/>
+	<link
+		rel="icon"
+		href="https://www.stpaulsschool.org.uk/wp-content/uploads/2019/05/St-Pauls-Logo-300x300.png"
+		sizes="192x192"
+	/>
+	<link
+		rel="apple-touch-icon"
+		href="https://www.stpaulsschool.org.uk/wp-content/uploads/2019/05/St-Pauls-Logo-300x300.png"
+	/>
 </svelte:head>
 
 {@render children?.()}
+
+<style>
+	:global(body),
+	:global(html) {
+		margin: 0;
+		padding: 0;
+
+		height: 100vh;
+		width: 100vw;
+	}
+
+	:global(*) {
+		box-sizing: border-box;
+	}
+</style>
--- sps-links/src/routes/+page.svelte
+++ sps-links/src/routes/+page.svelte
@@ -1,2 +1,113 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import { LINKS, MOBILE_LAYOUT, DESKTOP_LAYOUT } from '$lib/links.js';
+	import { onMount } from 'svelte';
+	let width = $state(0);
+	let isMobile = $derived(width < 768);
+	let layout = $derived(isMobile ? MOBILE_LAYOUT : DESKTOP_LAYOUT);
+	let linkContainerStyle = $derived(
+		`grid-template-columns: repeat(${layout[0]}, 1fr); grid-template-rows: repeat(${layout[1]}, 1fr);`
+	);
+</script>
+
+<svelte:window bind:innerWidth={width} />
+
+<div class="background"></div>
+
+<div class="link-container" style={linkContainerStyle}>
+	{#each LINKS as link}
+		<a
+			class="item"
+			href={link.link}
+			target="_blank"
+			style={link.background ? `background: ${link.background}` : ''}
+		>
+			{#if link.logo}
+				<img src={link.logo} alt={link.name} />
+			{/if}
+			{#if link.name}
+				<p>{link.name}</p>
+			{/if}
+		</a>
+	{/each}
+</div>
+
+<style>
+	@keyframes pan {
+		0% {
+			background-position: 0px 0%;
+		}
+
+		100% {
+			background-position: 2000px 0%;
+		}
+	}
+
+	.background {
+		position: absolute;
+		top: 0;
+		left: 0;
+		z-index: -1;
+		background: rgb(114, 9, 183);
+		background: linear-gradient(
+			90deg,
+			rgba(114, 9, 183, 1) 0%,
+			rgba(42, 13, 174, 1) 50%,
+			rgba(114, 9, 183, 1) 100%
+		);
+		background-size: 2000px 2000px;
+		width: 100vw;
+		height: 100vh;
+		animation: pan 30s linear infinite;
+	}
+
+	div.link-container {
+		padding: 0.5em;
+
+		width: 100%;
+		height: 100%;
+		z-index: 10;
+		display: grid;
+		gap: 0.5em;
+
+		.item {
+			justify-content: center;
+			align-items: center;
+			display: flex;
+			flex-direction: column;
+			position: relative;
+			border-radius: 20px;
+			padding: 2px;
+
+			background: rgba(0, 0, 0, 0.05);
+
+			transition: 0.4s linear;
+
+			img {
+				height: 50px;
+			}
+
+			p {
+				font-family: 'Nunito', 'Courier New', Courier, monospace;
+				color: hsl(0, 0%, 80%);
+				font-size: 20px;
+				font-weight: bold;
+				margin: 5px;
+				box-sizing: border-box;
+				text-decoration: none !important;
+			}
+		}
+
+		/* .item:has(img) p {
+        bottom: 10px;
+    } */
+
+		.item:hover {
+			background: rgba(0, 0, 0, 0.2);
+			transform: scale(1.05, 1.05);
+
+			p {
+				text-decoration: none;
+			}
+		}
+	}
+</style>
