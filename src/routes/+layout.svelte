19e2601bd088fc033c3c720af2c8b1ae7e9ab148
heheeheeha <maildrop0001@gmail.com>
2025-10-24 22:49:54 +0100

    speed optimisations

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,10 +5,22 @@
 <svelte:head>
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+
 	<link
+		rel="preload"
+		as="style"
 		href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap"
-		rel="stylesheet"
+		onload={() => {
+			this.onload = null;
+			this.rel = 'stylesheet';
+		}}
 	/>
+	<noscript>
+		<link
+			rel="stylesheet"
+			href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+		/>
+	</noscript>
 
 	<title>SPS Links</title>
 
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,20 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import Critters from 'critters';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		{
+			name: 'vite-plugin-critters',
+			enforce: 'post',
+			transformIndexHtml: {
+				enforce: 'post',
+				async transform(html) {
+					const critters = new Critters({ preload: 'swap', compress: true });
+					return await critters.process(html);
+				}
+			}
+		}
+	]
 });
