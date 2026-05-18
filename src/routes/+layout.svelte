4800ca42926cac3face384d4b922d00661810002
Heheheheeha <maildrop0001@gmail.com>
2026-05-18 20:43:19 +0100

    optimize font loading speed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,10 +5,8 @@
 <svelte:head>
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-	<link
-		href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap"
-		rel="stylesheet"
-	/>
+	<link rel="preload" href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" as="style" onload={() => { this.onload = null; this.rel = 'stylesheet'; }}>
+	<noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap"></noscript>
 </svelte:head>
 
 {@render children?.()}
