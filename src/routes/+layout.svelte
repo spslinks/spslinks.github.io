0a5f172773eb69d53f9cdcf745b1d39a4615d197
heheheha <maildrop0001@gmail.com>
2026-09-03 20:38:32 +0100

    move font to local

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,10 +3,6 @@
 </script>
 
 <svelte:head>
-	<link rel="preconnect" href="https://fonts.googleapis.com" />
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-	<link rel="preload" href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" as="style" onload={() => { this.onload = null; this.rel = 'stylesheet'; }}>
-	<noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap"></noscript>
 </svelte:head>
 
 {@render children?.()}
